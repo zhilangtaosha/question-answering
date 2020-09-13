982c4c8ddb59b9bc8d3e2190592468b14549c8ea
bogao 00563677 <bogao@huawei.com>
2020-09-13 10:14:16 +0200

    'add'

--- runs/run_ES_DPR_Reader.py
+++ runs/run_ES_DPR_Reader.py
@@ -12,10 +12,8 @@ from eval import *
 DATA_DIR = join('..', 'data')
 DATASETS = [
     join(DATA_DIR, 'squad2', 'squad2-dev.json'),
-    # join(DATA_DIR, 'quasarT', 'quasarT-dev.json'),
-    # join(DATA_DIR, 'searchQA', 'searchQA-dev.json'),
-    # join(DATA_DIR, 'triviaQA', 'triviaQA-dev.json'),
-    # join(DATA_DIR, 'wikiQA', 'wikiQA-dev.json')
+    join(DATA_DIR, 'naturalQuestions', 'naturalQuestions-dev-clean.json'),
+    join(DATA_DIR, 'wikiQA', 'wikiQA-test.json'),
 ]
 RETRIEVER_ES_TOP_K = 1000
 RETRIEVER_DPR_TOP_K = 20
