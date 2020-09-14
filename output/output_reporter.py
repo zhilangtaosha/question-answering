634ecec0b49ce302d70a4c4959287916e17ddc21
bo <bo.gao111@gmail.com>
2020-09-14 21:48:44 +0200

    add

--- output/output_reporter.py
+++ output/output_reporter.py
@@ -1,4 +1,3 @@
-import json
 import sys, os
 sys.path.insert(1, os.path.join('..', 'common'))
 from utils import *
@@ -13,17 +12,18 @@ class OutputReporter:
         with open(output_path, 'r', encoding='utf8') as f:
             self.items = json.load(f)
 
-    def get_recalls(self, field):
+    def get_recalls(self, field, start=5, end=100, step=5):
+        recalls = []
         if self.items:
             assert field in self.items[0]
             ranks_list = [item[field] for item in self.items]
-
+            for k in range(start, end, step):
+                r = recall_at_k(ranks_list, k)
+                recalls.append((k, r))
+        return recalls
 
 
 if __name__ == '__main__':
     example_path = 'wiki_50/qa_BM25_wikipedia_50_5000_naturalQuestions-dev-clean_1000.json'
     reporter = OutputReporter(example_path)
     print(reporter.items[0])
-
-
-    # round(recall_at_k(bm25_ranks_list, 5), round_num)
